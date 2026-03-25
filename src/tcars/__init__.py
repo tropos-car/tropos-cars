e279699f2fadf4ffff6961e97e624f0b0eb62132
jonas-witthuhn <69144445+jonas-witthuhn@users.noreply.github.com>
2026-03-25 07:54:02 +0100

    add ecrad version

--- src/tcars/__init__.py
+++ src/tcars/__init__.py
@@ -1,7 +1,15 @@
+__version__ = ""
 
 from importlib.metadata import version, PackageNotFoundError
 try:
-    __version__ = version("tcars")
+    __version__ += version("tcars")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+try:
+    import _ecrad_version
+    __version__ += f", ecrad-{_ecrad_version.__version__}"
 except PackageNotFoundError:
     # package is not installed
     pass
